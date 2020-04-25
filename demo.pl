64a0ddec6958baf5a478b73dfd8bfda9db8421a9
KShivendu <shivendu@iitbhilai.ac.in>
2020-04-25 23:26:11 +0530

    Added hash and array variable identification

--- demo.pl
+++ demo.pl
@@ -24,3 +24,25 @@ print "Geeks       for
     print "hi-";
     print("bye\n");
 }
+
+=Correct way to declare varialbes in perl
+$my-name = "John"; # Invalid 
+$my name = "John"; # Invalid
+$my_name = "John"; # Valid
+=cut
+
+# Array Variables
+@price_list = (70, 30, 40);
+
+# Hash Variables
+%item_pairs = ("Apple" =>2, "Banana"=>3);
+%pair_random = ("Hi" =>8, "Bye"=>9);
+
+@price_list[2] = "4";
+
+$item_pairs{"Apple"} = 212;
+
+
+$name = "John"
+print 'Hi $name\nHow are you?' 
+
